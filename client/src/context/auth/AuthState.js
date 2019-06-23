b9e7b2f4990f74d14581be1653dc1099ea537044
Janet Warren <jewljbl13@aol.com>
2019-06-23 00:49:54 -0400

    add auth for user login

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -54,8 +54,19 @@ const AuthState = props => {
     }
   };
   //Login User
-  const login = () => {
-    console.log('User Logged In');
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    try {
+      const res = await axios.post('api/auth', formData, config);
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+      loadUser();
+    } catch (err) {
+      dispatch({ type: LOGIN_FAIL, payload: err.response.data.msg });
+    }
   };
   // Logout
   const logout = () => {
